f3d1204a9f06cf73cb0e7235cadb795a232f20cf
Ryan <ryan6tk@hotmail.co.uk>
2017-03-18 23:53:31 +0000

    Rolled back ES6 stuff due to ios incompatibility.

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ Constant.FEATURED_IMAGE_FILENAMES = ['wip.png', 'wip.png'];
 Constant.RECENT_IMAGE_FILENAMES = ['moniac-bw.png', 'dx11-bw.png', 'website-bw.png', 'c-bw.png', 'stats-bw.png', 'pw-bw.png', 'water-bw.png', 'ab-bw.png', 'fps-bw.png', 'chat-bw.png', 'work-bw.png', 'hush-bw.png', 'dx9-bw.png'];
 Constant.FEATURED_FILENAME_PREFIX = 'f';
 
-const contentArea = document.getElementsByClassName('content')[0];
-const tabs = document.getElementsByClassName('navButton');
+var contentArea = document.getElementsByClassName('content')[0];
+var tabs = document.getElementsByClassName('navButton');
 
-let tiles = [];
-let scrollElement;
+var tiles = [];
+var scrollElement;
 
-let timeoutHandle;
-let intervalHandle;
+var timeoutHandle;
+var intervalHandle;
 
 /* Basic http request. */
 function httpRequest(url, callback) {
-	const xmlHttp = new XMLHttpRequest();
+	var xmlHttp = new XMLHttpRequest();
    	xmlHttp.onreadystatechange = function() { 
         	if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
         	    	callback(xmlHttp.responseText);
@@ -45,10 +45,10 @@ function fade() {
 }
 
 /* Switches to the given tab by changing classes. */
-let activeTab;
-let activeTabIndex = -1;
+var activeTab;
+var activeTabIndex = -1;
 function changeActiveTab(index) {
-	const isTabAlreadyActive = index === activeTabIndex;
+	var isTabAlreadyActive = index === activeTabIndex;
 	if (isTabAlreadyActive) {
 		return; // Don't bother switching to a tab we are already on
 	}
@@ -66,7 +66,7 @@ function changeActiveTab(index) {
 	fade();
 	httpRequest(Constant.TAB_URLS[index], function(html) {
 		// Clean up any tiles on the page
-		for (const tile of tiles) {
+		for (var tile of tiles) {
 			tile.removeEventListener('click', tileClick);
 		}
 		tiles = [];
@@ -74,99 +74,115 @@ function changeActiveTab(index) {
 		contentArea.innerHTML = html;
 		
 		// Extra setup is only required on pages containing tiles
-		const isTilePage = index === Constant.FEATURED_PAGE_INDEX || index === Constant.RECENT_PAGE_INDEX;
+		var isTilePage = index === Constant.FEATURED_PAGE_INDEX || index === Constant.RECENT_PAGE_INDEX;
 		if (!isTilePage) {
 			return;
 		}
 		
-		const isFeaturedPage = activeTabIndex === Constant.FEATURED_PAGE_INDEX;
+		var isFeaturedPage = activeTabIndex === Constant.FEATURED_PAGE_INDEX;
 		scrollElement = isFeaturedPage ? document.getElementsByClassName("tile-ContainerLarge")[0] : document.getElementsByClassName("tile-Container")[0];
 		
 		// Each tile has a loading css animation which plays until its image is loaded. Create a background image and set up load behaviour.
-		const imagesToLoad = isFeaturedPage ? Constant.FEATURED_IMAGE_FILENAMES : Constant.RECENT_IMAGE_FILENAMES;
-		const imageElements = document.getElementsByClassName('tile-TileImage');
-		const backgroundImages = [];
-		const imageCount = imageElements.length;
-		for (let i = 0; i < imageCount; i++) {
-			const backgroundImage = new Image();
-			backgroundImage.classList.add('tile-LoadedImage');
-			backgroundImage.onload = function() {
-				const currentParent = imageElements[i];
-				currentParent.innerHTML = ''; // Clear the loading animation
-				currentParent.appendChild(backgroundImages[i]); // Image has now finished loading so append it
-				delete backgroundImages[i]; // Remvoe the image from memory
-			}
-			backgroundImage.src = Constant.ASSETS_DIRECTORY + imagesToLoad[i];
-			backgroundImages.push(backgroundImage);
+		var imagesToLoad = isFeaturedPage ? Constant.FEATURED_IMAGE_FILENAMES : Constant.RECENT_IMAGE_FILENAMES;
+		var imageElements = document.getElementsByClassName('tile-TileImage');
+		var backgroundImages = [];
+		var imageCount = imageElements.length;
+		for (var i = 0; i < imageCount; i++) {
+			(function(index) {
+				var backgroundImage = new Image();
+				backgroundImage.classList.add('tile-LoadedImage');
+				backgroundImage.onload = function() {
+					var currentParent = imageElements[index];
+					currentParent.innerHTML = ''; // Clear the loading animation
+					currentParent.appendChild(backgroundImages[index]); // Image has now finished loading so append it
+					delete backgroundImages[index]; // Remvoe the image from memory
+				}
+				backgroundImage.src = Constant.ASSETS_DIRECTORY + imagesToLoad[i];
+				backgroundImages.push(backgroundImage);
+			})(i)
 		}
 		
 		// Set up tile click behaviour
 		tiles = document.getElementsByClassName('tile-Tile');
-		for (const tile of tiles) {
+		for (var tile of tiles) {
 			tile.addEventListener('click', tileClick);
 		}
 	});
 }
 
-
-let activeTileIndex = -1;
-let clickedTile;
-let clickedTileHTML;
+var activeTileIndex = -1;
+var clickedTile;
+var clickedTileHTML;
 function tileClick() {
 	clickedTile = this;
-	const tileIndex = clickedTile.firstElementChild.textContent; // Each tile's first child is a span containing the numerical position of the tile
+	var tileIndex = clickedTile.firstElementChild.textContent; // Each tile's first child is a span containing the numerical position of the tile
 	
 	// Deselect clicked tile if it was already active
-	const isTileAlreadyActive = tileIndex === activeTileIndex;
+	var isTileAlreadyActive = tileIndex === activeTileIndex;
 	if (isTileAlreadyActive) {
 		activeTileIndex = -1;
-		clickedTile.classList.remove(Constant.ACTIVE_TILE_CLASS);
-		clickedTile.innerHTML = clickedTileHTML;
+		closeTile(this);
 		return;
 	};
 	
 	// Remove active tile styling from currently active tile
-	const activeTileExists = activeTileIndex > -1;
+	var activeTileExists = activeTileIndex > -1;
 	if (activeTileExists) {
-		const activeTile = tiles[activeTileIndex];
-		activeTile.classList.remove(Constant.ACTIVE_TILE_CLASS);
-		activeTile.innerHTML = clickedTileHTML;
+		var activeTile = tiles[activeTileIndex];
+		closeTile(activeTile);
 	}
 	
-	clickedTile.classList.add(Constant.ACTIVE_TILE_CLASS);
+	openTile(this, tileIndex);
+}
+
+function closeTile(tile) {
+	clickedTile.classList.remove(Constant.ACTIVE_TILE_CLASS);
+	clickedTile.innerHTML = clickedTileHTML;
+
+	if (intervalHandle) {
+		clearInterval(intervalHandle);
+	}
+
+	if (timeoutHandle) {
+		clearTimeout(timeoutHandle);
+	}
+}
+
+function openTile(tile, tileIndex) {
+	tile.classList.add(Constant.ACTIVE_TILE_CLASS);
 	activeTileIndex = tileIndex;
 	
-	const isFeaturedPage = activeTabIndex === Constant.FEATURED_PAGE_INDEX;
-	const prefix = isFeaturedPage ? Constant.FEATURED_FILENAME_PREFIX : '';
+	var isFeaturedPage = activeTabIndex === Constant.FEATURED_PAGE_INDEX;
+	var prefix = isFeaturedPage ? Constant.FEATURED_FILENAME_PREFIX : '';
 	
 	httpRequest(Constant.TILES_DIRECTORY + tileIndex + '.html', function(html) {
-		clickedTileHTML = clickedTile.innerHTML;
-		clickedTile.innerHTML = html;
+		clickedTileHTML = tile.innerHTML;
+		tile.innerHTML = html;
 	});
 	
 	// After a second the tile will be fully expanded, so begin scrolling it into view
 	timeoutHandle = setTimeout(function() {
 		intervalHandle = setInterval(function() {
-			const differenceY = scrollElement.scrollTop - clickedTile.offsetTop;
-			const shouldScrollDown = differenceY < 0;
-			const scrollComplete = shouldScrollDown ? differenceY >= -10 : differenceY < 10;
+			var differenceY = scrollElement.scrollTop - tile.offsetTop;
+			var shouldScrollDown = differenceY < 0;
+			var scrollCompvare = shouldScrollDown ? differenceY >= -10 : differenceY < 10;
 			if (shouldScrollDown) {
-				if (scrollComplete) {
+				if (scrollCompvare) {
 					clearInterval(intervalHandle);
 				}
 				else if (differenceY >= -30) {
-					scrollElement.scrollTop = clickedTile.offsetTop - 10;
+					scrollElement.scrollTop = tile.offsetTop - 10;
 				}
 				else {
 					scrollElement.scrollTop = scrollElement.scrollTop + 30;
 				}
 			}
 			else {
-				if (scrollComplete) {
+				if (scrollCompvare) {
 					clearInterval(intervalHandle);
 				}
 				else if (differenceY <= 30) {
-					scrollElement.scrollTop = clickedTile.offsetTop - 10;
+					scrollElement.scrollTop = tile.offsetTop - 10;
 				}
 				else {
 					scrollElement.scrollTop = scrollElement.scrollTop - 30;
@@ -181,7 +197,7 @@ function tileClick() {
 }
 
 // Program Start
-for (const tab of tabs) {
+for (var tab of tabs) {
 	tab.addEventListener('click', function() {
 		changeActiveTab(parseInt(this.id));
 	});
